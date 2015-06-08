73b52d5ff098afe9f960a470e45a58bb70c3928a
jcmcnalx <jmcnall3@asu.edu>
2015-06-08 14:49:26 -0700

    run against Dockerfile in same folder to add uids and gids

--- identity.sh
+++ identity.sh
@@ -4,6 +4,3 @@ export uid=${uid} group=$(id -g) username=$(id -un)
 sed -i "s/userid/${uid}/g" Dockerfile
 sed -i "s/groupid/$group/g" Dockerfile
 sed -i "s/username/$username/g" Dockerfile
-#echo ${uid} > uid
-#echo $group > gid
-#echo $username > user
